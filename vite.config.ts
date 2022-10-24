c92ffc4033c5bae1435aec51e188f118ce56b708
Suxin <208082474@qq.com>
2022-10-24 23:04:04 +0800

    lbt

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import ElementPlus from 'vite-plugin-element-plus'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import ElementPlus from "vite-plugin-element-plus";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),ElementPlus({ 
-  })], 	server:{
-		proxy:{
-			'/api':'http://testapi.xuexiluxian.cn'
-		}
-	},
-  base:'./' // 添加这个属性
-})
+  plugins: [vue(), ElementPlus({})],
+  server: {
+    // proxy: {
+    //   "/api": "http://testapi.xuexiluxian.cn",
+    // },
+  },
+  base: "./", // 添加这个属性
+});
