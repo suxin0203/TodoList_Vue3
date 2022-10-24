4bf8ebece03450ddfd99de2827bdc5b3ddeffa44
Suxin <208082474@qq.com>
2022-10-24 23:04:44 +0800

    111

--- src/api/slider.js
+++ src/api/slider.js
@@ -2,7 +2,7 @@ import request from '../utils/request'
 
 export function getSliders( ){
 	return request({
-		url:'/api/slider/getSliders',
+		url:'http://testapi.xuexiluxian.cn/api/slider/getSliders',
 
 	})
 }
